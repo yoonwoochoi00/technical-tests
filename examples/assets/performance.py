41e23ef3436e59fff6df519c383766e4364026aa
yunuchoi621 <91050411+yunuchoi621@users.noreply.github.com>
2023-04-11 18:21:34 +1200

    Fixed typo in add()
    "Ineffient" to "Inefficient"

--- examples/assets/performance.py
+++ examples/assets/performance.py
@@ -17,7 +17,7 @@ class PerformanceTests:
 
     def add(self, value_to_add: int):
         """
-        Ineffient way of adding values to a collection.
+        Inefficient way of adding values to a collection.
 
         Args:
             value_to_add (int): The value to add
